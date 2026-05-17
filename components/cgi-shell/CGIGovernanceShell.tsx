cdbd68ab0e4539b39e524dc2dc405241219f3cdb
Everisto Mapfidze <mapfidzee@gmail.com>
2026-05-16 23:25:18 -0400

    Tighten CGI governance shell content layout

--- components/cgi-shell/CGIGovernanceShell.tsx
+++ components/cgi-shell/CGIGovernanceShell.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react'
+
 import CGISidebar from './CGISidebar'
 import CGITopbar from './CGITopbar'
 
@@ -12,15 +13,15 @@ export default function CGIGovernanceShell({
   return (
     <div className="min-h-screen bg-neutral-950 text-neutral-100">
       <div className="flex min-h-screen">
-        <div className="hidden w-80 shrink-0 lg:block">
+        <aside className="hidden w-80 shrink-0 border-r border-neutral-800 bg-neutral-950 lg:block">
           <CGISidebar />
-        </div>
+        </aside>
 
-        <div className="flex min-h-screen flex-1 flex-col">
+        <div className="flex min-h-screen min-w-0 flex-1 flex-col">
           <CGITopbar />
 
-          <main className="flex-1 bg-neutral-950 px-4 py-6 sm:px-6 lg:px-8">
-            <div className="mx-auto w-full max-w-7xl">{children}</div>
+          <main className="flex-1 bg-neutral-950 px-4 py-6 sm:px-6 lg:px-6">
+            {children}
           </main>
         </div>
       </div>
